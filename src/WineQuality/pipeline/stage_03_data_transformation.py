15b77525608f73ec07a96fd60f86571c2adcda18
arham <arhamop937@gmail.com>
2025-12-09 17:38:43 +0500

    data transformation pipeline

---
+++ src/WineQuality/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,18 @@
+from WineQuality.config.configuration import ConfigurationManager
+from WineQuality.components.data_transformation import DataTransformation
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
+        
+        
+        
