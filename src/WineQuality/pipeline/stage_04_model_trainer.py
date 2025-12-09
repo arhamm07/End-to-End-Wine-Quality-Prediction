a6dad7c16152e5a10e95b4b0baa37413c2cab879
arham <arhamop937@gmail.com>
2025-12-09 17:39:14 +0500

    model training pipeline

---
+++ src/WineQuality/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,16 @@
+from WineQuality.config.configuration import ConfigurationManager
+from WineQuality.components.model_trainer import ModelTrainer
+
+
+STAGE_NAME = "Model Training Stage"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer = ModelTrainer(config=model_trainer_config)
+        model_trainer.train()
+        
